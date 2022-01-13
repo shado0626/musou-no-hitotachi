e41621107b0aabf1d9852c7e849fe37c6b07e14e
Sherin Binu <sherinbinu@hotmail.com>
2022-01-13 21:21:41 +0900

    add Base class

---
+++ src/Base.ts
@@ -0,0 +1,45 @@
+export interface IBase {
+  id: string;
+  hp: number;
+  atk: number;
+  def: number;
+  atkPer: number;
+  cRate: number;
+  cDmg: number;
+  phyDmg: number;
+  eleDmg: number;
+  er: number;
+  defPer: number;
+  hpPer: number;
+  em: number;
+  healing: number;
+}
+
+export abstract class Base {
+  private data: IBase[] = [
+    {
+      id: 'RaidenShogun',
+      hp: 12907,
+      atk: 337,
+      def: 789,
+      atkPer: 0,
+      cRate: 0,
+      cDmg: 0,
+      phyDmg: 0,
+      eleDmg: 0,
+      er: 0.32,
+      defPer: 0,
+      hpPer: 0,
+      em: 0,
+      healing: 0,
+    },
+  ];
+
+  getBase(charId: string): IBase {
+    const char: IBase | undefined = this.data.find(
+      (item: IBase) => item.id === charId
+    );
+    if (char) return char;
+    throw new Error('id doesnt exist');
+  }
+}
