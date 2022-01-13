4bb137fc37c7e96deba3a060b2180d13dd453346
Sherin Binu <sherinbinu@hotmail.com>
2022-01-13 23:08:13 +0900

    refactor DPSer class

---
+++ src/DPSer.ts
@@ -0,0 +1,37 @@
+import charJson from './data/characters.json';
+export interface IDPSer {
+  id: string;
+  hp: number;
+  atk: number;
+  def: number;
+  atkPer: number;
+  cRate: number;
+  cDmg: number;
+  phyDmg: number;
+  eleDmg: number;
+  er: number;
+  defPer: number;
+  hpPer: number;
+  em: number;
+  healing: number;
+  level: number;
+}
+
+export class DPSer {
+  private data: IDPSer[] = charJson;
+  private character: IDPSer = this.data.find(
+    (i: IDPSer) => i.id === 'RaidenShogun'
+  ) as IDPSer;
+
+  setChar(charId: string): void {
+    const char: IDPSer | undefined = this.data.find(
+      (item: IDPSer) => item.id === charId
+    );
+    if (char) this.character = char;
+    throw new Error('character doesnt exist');
+  }
+
+  get char(): IDPSer {
+    return this.character;
+  }
+}
