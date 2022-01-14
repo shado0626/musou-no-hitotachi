6dacfce3f297453c587475d7d72b7196f8572626
Sherin Binu <sherinbinu@hotmail.com>
2022-01-14 15:53:49 +0900

    implement basic Artifacts class

--- src/Artifacts.ts
+++ src/Artifacts.ts
@@ -10,38 +10,85 @@ export interface IArtifact {
   rolls: number[];
 }
 
+const mainStats = {
+  er: 0.518,
+  atk: 0.466,
+  ele: 0.466,
+  cd: 0.622,
+};
+
+const subStats = {
+  cd: 0.0777,
+  er: 0.0648,
+  atk: 0.0583,
+  em: 23.31,
+};
+
 export class Artifacts {
   private data: IArtifactsSet[] = artiJson.sets;
-  private artifactSet: IArtifactsSet = this.data.find(
-    (i: IArtifactsSet) => i.id === '4eosf'
-  ) as IArtifactsSet;
-  private sands: IArtifact = {
-    main: 'er',
-    subs: ['atk', 'flatAtk', 'cd'],
-    rolls: [0, 0, 0],
-  };
-  private goblet: IArtifact = {
-    main: 'ele',
-    subs: ['atk', 'flatAtk', 'cd', 'er'],
-    rolls: [0, 0, 0, 0],
-  };
-  private circlet: IArtifact = {
-    main: 'cd',
-    subs: ['atk', 'flatAtk', 'er'],
-    rolls: [0, 0, 0],
-  };
+  private artifactSet: IArtifactsSet;
+  private flower: IArtifact;
+  private feather: IArtifact;
+  private sands: IArtifact[];
+  private goblet: IArtifact[];
+  private circlet: IArtifact[];
 
-  setArtifactsSet(artiId: string): void {
-    const arti: IArtifactsSet | undefined = this.data.find(
-      (item: IArtifactsSet) => item.id === artiId
-    );
-    if (arti) this.artifactSet = arti;
-    else throw new Error('artifacts set doesnt exist');
+  constructor(setId: string, mains: string[], subs: string[]) {
+    const arti: IArtifactsSet = this.data.find(
+      (item: IArtifactsSet) => item.id === setId
+    ) as IArtifactsSet;
+    this.artifactSet = arti;
+
+    const flowerSubs = subs.filter((i) => i !== 'flatHp');
+    const featherSubs = subs.filter((i) => i !== 'flatAtk');
+
+    this.flower = {
+      main: 'flatHp',
+      subs: flowerSubs,
+      rolls: new Array(flowerSubs.length).fill(0),
+    };
+    this.feather = {
+      main: 'flatAtk',
+      subs: featherSubs,
+      rolls: new Array(featherSubs.length).fill(0),
+    };
+    this.sands = mains
+      .filter((i) => artiJson.sands.includes(i))
+      .map((i) => {
+        const s = subs.filter((j) => j !== i);
+        return {
+          main: i,
+          subs: s,
+          rolls: new Array(s.length).fill(0) as number[],
+        };
+      });
+    this.goblet = mains
+      .filter((i) => artiJson.goblet.includes(i))
+      .map((i) => {
+        const s = subs.filter((j) => j !== i);
+        return {
+          main: i,
+          subs: s,
+          rolls: new Array(s.length).fill(0) as number[],
+        };
+      });
+    this.circlet = mains
+      .filter((i) => artiJson.circlet.includes(i))
+      .map((i) => {
+        const s = subs.filter((j) => j !== i);
+        return {
+          main: i,
+          subs: s,
+          rolls: new Array(s.length).fill(0) as number[],
+        };
+      });
   }
 
-  setArtifacts(sands: IArtifact, goblet: IArtifact, circlet: IArtifact): void {
-    this.sands = sands;
-    this.goblet = goblet;
-    this.circlet = circlet;
+  printData(): void {
+    console.log(this.flower);
+    console.log(this.feather);
+    console.log(this.sands);
+    console.log(this.goblet);
+    console.log(this.circlet);
   }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import { DPSer } from './DPSer';
 import { Weapon } from './Weapon';
+import { Artifacts } from './Artifacts';
 
-const dpser = new DPSer();
-const weapon = new Weapon();
+const dpser = new DPSer('Ganyu');
+const weapon = new Weapon('R5Amos');
+const artifacts = new Artifacts(
+  '4sr',
+  ['em', 'cd', 'ele', 'atk'],
+  ['em', 'atk', 'cd', 'flatAtk']
+);
 
-dpser.setChar('Ganyu');
-weapon.setWeapon('R5Amos');
+artifacts.printData();
